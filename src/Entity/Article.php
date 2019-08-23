86b8e90ebcef179a93cba75603598ff099fd300e
Harun Baş <harun.bas@enuygun.com>
2019-08-23 11:13:32 +0300

    queries added.

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,8 +2,10 @@
 
 namespace App\Entity;
 
+use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 
@@ -209,6 +211,16 @@ class Article
     public function getComments(): Collection
     {
         return $this->comments;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getNonDeletedComments(): Collection
+    {
+        $criteria = ArticleRepository::createNonDeletedCriteria();
+
+        return $this->comments->matching($criteria);
     }
 
     public function  addComment(Comment $comment): self
